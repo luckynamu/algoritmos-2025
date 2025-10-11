acfdd794219bc2e113549885bb9a7fe956751179
luckynamu <mpasotti@frba.utn.edu.ar>
2025-10-11 10:35:50 -0300

    TP integrador

---
+++ 2° Parcial/Ejercicio de parcial 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    FILE *f;
+    int x;
+    int v1[10] = {101, 102, 103, 104, 105, 106};
+    int v2[5] = {11, 12, 13, 14, 15};
+    f = fopen("C:/Users/mvpas/Documents/Universidad/Algoritmos y Estructuras de Datos/Archivos/parcial.data", "wb+");
+        fwrite(v1, sizeof(int), 10, f);
+        fclose(f);
+    f = fopen("C:/Users/mvpas/Documents/Universidad/Algoritmos y Estructuras de Datos/Archivos/parcial.data", "rb+");
+        fread(&x, sizeof(int), 1, f);
+        fread(v1, sizeof(int), 4, f);
+        fseek(f, (-3)*sizeof(int), SEEK_CUR);
+        fwrite(v2+1, sizeof(int), 3, f);
+        fclose(f);
+
+    f = fopen("C:/Users/mvpas/Documents/Universidad/Algoritmos y Estructuras de Datos/Archivos/parcial.data", "rb");
+    while ((fread(&x, sizeof(int), 1, f)) == 1){
+        cout << x << endl;
+    }
+    fclose (f);
+    return 0;
+}
---
+++ Guía N°5 (Punteros, pila y estructuras dinamicas)/Ejercicio 1.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+//Declara un puntero a entero y asígnale la dirección de una variable.
